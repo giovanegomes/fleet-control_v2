8105bae61806a6354b1a15774876769478fdd08c
Giovane Aires Gomes <giovaneag17@outlook.com>
2026-02-14 12:31:20 -0300

    feat: adds UUID validation and improves error handling

--- src/core/services/vehicle.service.ts
+++ src/core/services/vehicle.service.ts
@@ -3,6 +3,7 @@ import {
   updateVehicleSchema,
 } from "core/validators/vehicle.schema";
 import { vehicleRepository } from "../repositories/vehicle.repository";
+import { uuidSchema } from "core/db/schema/validators";
 
 export async function getVehicles() {
   return vehicleRepository.get();
@@ -15,11 +16,22 @@ export async function createVehicle(data: unknown) {
 }
 
 export async function updateVehicle(id: string, data: unknown) {
+  const validId = uuidSchema.parse(id);
   const vehicle = updateVehicleSchema.parse(data);
+  const result = await vehicleRepository.update(validId, vehicle);
 
-  return vehicleRepository.update(id, vehicle);
+  if (result.length === 0) {
+    throw new Error("Vehicle not found.");
+  }
+
+  return result[0];
 }
 
 export async function deleteVehicle(id: string) {
-  return vehicleRepository.delete(id);
+  const validId = uuidSchema.parse(id);
+  const result = await vehicleRepository.delete(validId);
+
+  if (result.length === 0) {
+    throw new Error("Vehicle not found.");
+  }
 }
